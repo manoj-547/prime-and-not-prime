0e1fd321fd4f22a69c9fbab0e3f1a600cf513a2a
B.Manoj <66582080+manoj-547@users.noreply.github.com>
2022-05-27 12:17:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# num=float(input('Enter the number:'))
+# i=2
+# while i<=num-1:
+#     if num%i==0:
+#         print(num,' is not a prime number')
+#         break
+#     i+=1
+# else:
+#     print(num,' is a prime number')
